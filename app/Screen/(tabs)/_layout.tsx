760b5a9661df74bf02c54be001af20fe3bd3c243
Hirdesh Keswani <hirdeshkeshwani2001@gmail.com>
2026-02-03 18:16:27 +0530

    added auth slice

--- app/Screen/(tabs)/_layout.tsx
+++ app/Screen/(tabs)/_layout.tsx
@@ -6,9 +6,10 @@ import { View,Image } from "react-native";
 import HomeIcon from "../../../assets/icons/icons8-home-100.png";
 import OrderIcon from "../../../assets/icons/icons8-order-100.png";
 import CartIcon from "../../../assets/icons/icons8-cart-100.png";
+import {useSelector} from "react-redux";
 export default function rootLayout(){
     const Tab = createBottomTabNavigator();
-
+    let itemCount = useSelector((state:RootState)=>state.cart.noOfItems);
     return (
         <Tab.Navigator screenOptions={{headerShown:false,tabBarShowLabel:true}}>
             <Tab.Screen 
@@ -30,7 +31,8 @@ export default function rootLayout(){
                     <View style={{padding:6,borderRadius:18,backgroundColor:focused?'#007AFF' : 'transparent'}}>
                         <Image source={CartIcon} style={{height:24,width:24,tintColor:focused ? 'white' : '#8e8e93'}}/>
                     </View>
-                )
+                ),
+                tabBarBadge:(itemCount>0)?itemCount:undefined
              }}
 
              />
--- app/slices/cartslice.ts
+++ app/slices/cartslice.ts
@@ -9,11 +9,13 @@ interface Product{
 interface ProductState{
     product:Product[],
     totalPrice:number;
+    noOfItems:number;
 }
 
 const initialState:ProductState = {
     product:[],
-    totalPrice:0
+    totalPrice:0,
+    noOfItems:0
 }
 
 const cartSlice = createSlice({
@@ -44,8 +46,101 @@ const cartSlice = createSlice({
                 }
             }
         },
-        deleteItem(state,action){}
+        deleteItem(state,action){},
+        totalItems(state,action){
+            let itemsCount = state.product.length;
+            state.noOfItems = itemsCount;
+        }
     }
 });
-export const {addItem,increaseQuantity,decreaseQuantity,calculatePrice,deleteItem} = cartSlice.actions;
+export const {addItem,increaseQuantity,decreaseQuantity,totalItems,calculatePrice,deleteItem} = cartSlice.actions;
 export default cartSlice.reducer;
+
+
+
+
+
+
+// const updateCartSummary = (state: CartState) => {
+//   state.totalPrice = state.product.reduce(
+//     (total, item) => total + item.productPrice * item.productQuantity,
+//     0
+//   );
+
+//   state.noOfItems = state.product.reduce(
+//     (count, item) => count + item.productQuantity,
+//     0
+//   );
+// };
+
+
+
+
+
+
+
+
+
+// import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+// interface Product {
+//   productId: string;
+//   productName: string;
+//   productPrice: number;
+//   productQuantity: number;
+// }
+
+// interface CartState {
+//   product: Product[];
+//   totalPrice: number;
+//   noOfItems: number;
+// }
+
+// const initialState: CartState = {
+//   product: [],
+//   totalPrice: 0,
+//   noOfItems: 0,
+// };
+
+// const cartSlice = createSlice({
+//   name: "cart",
+//   initialState,
+//   reducers: {
+//     addItem(state, action: PayloadAction<Product>) {
+//       const item = state.product.find(
+//         p => p.productId === action.payload.productId
+//       );
+
+//       if (item) {
+//         item.productQuantity += 1;
+//       } else {
+//         state.product.push(action.payload);
+//       }
+
+//       updateCartSummary(state);
+//     },
+
+//     increaseQuantity(state, action: PayloadAction<string>) {
+//       const item = state.product.find(p => p.productId === action.payload);
+//       if (item) {
+//         item.productQuantity += 1;
+//       }
+//       updateCartSummary(state);
+//     },
+
+//     decreaseQuantity(state, action: PayloadAction<string>) {
+//       const item = state.product.find(p => p.productId === action.payload);
+//       if (item && item.productQuantity > 1) {
+//         item.productQuantity -= 1;
+//       }
+//       updateCartSummary(state);
+//     },
+
+//     deleteItem(state, action: PayloadAction<string>) {
+//       state.product = state.product.filter(
+//         p => p.productId !== action.payload
+//       );
+//       updateCartSummary(state);
+//     },
+//   },
+// });
